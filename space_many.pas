f0d7040579bd660f9e88a33d1843ffb58aa82d11
Endg4meZer0 <timka.tarasen@gmail.com>
2024-05-22 16:02:40 +1000

    доливаю немного фиксов для валидации

--- space_many.pas
+++ space_many.pas
@@ -28,20 +28,24 @@ var err_string: string;
 begin
   err_string := '';
   
-  if s[1] = ' ' then append_err(err_string, 'ФОРМАТ ДАННЫХ: Строка не должна начинаться с пробела.');
-  if s[s.Length] = ' ' then append_err(err_string, 'ФОРМАТ ДАННЫХ: Строка не должна оканчиваться на пробел.');
-  
-  count := 0;
-  for i := 1 to s.Length do begin
-    if (s[i] = ' ') and not space_flag then begin
-      space_flag := true;
-      count := count + 1;
-    end
-    else space_flag := false;
+  if s.Length = 0 then append_err(err_string, 'ФОРМАТ ДАННЫХ: Обнаружена пустая строка.')
+  else begin
+    if s[1] = ' ' then append_err(err_string, 'ФОРМАТ ДАННЫХ: Строка не должна начинаться с пробела.');
+    if s[s.Length] = ' ' then append_err(err_string, 'ФОРМАТ ДАННЫХ: Строка не должна оканчиваться на пробел.');
+    
+    count := 0;
+    for i := 1 to s.Length do begin
+      if (s[i] = ' ') and not space_flag then begin
+        space_flag := true;
+        count := count + 1;
+      end
+      else space_flag := false;
+    end;
+    if (count > max_elements_count) then append_err(err_string, 'ФОРМАТ ДАННЫХ: Слишком много данных. Максимальное количество товаров: ' + ((max_elements_count - 4) / 2).ToString() + ', максимальное кол-во полей в общем: ' + max_elements_count.ToString() + '.')
+    else if (count < 6) then append_err(err_string, 'ФОРМАТ ДАННЫХ: Слишком мало данных. Необходимо 4 обязательных поля (день, месяц и год отгрузки и код заказа), а также хотя бы один товар (два поля: код и кол-во).')
+    else if (count mod 2 = 1) then append_err(err_string, 'ФОРМАТ ДАННЫХ: Неверное количество данных. Товары имеют по два поля: код и кол-во. У последнего товара указан только код.');
   end;
-  if (count > max_elements_count) then append_err(err_string, 'ФОРМАТ ДАННЫХ: Слишком много данных. Максимальное количество товаров: ' + ((max_elements_count - 4) / 2).ToString() + ', максимальное кол-во полей в общем: ' + max_elements_count.ToString() + '.')
-  else if (count < 6) then append_err(err_string, 'ФОРМАТ ДАННЫХ: Слишком мало данных. Необходимо 4 обязательных поля (день, месяц и год отгрузки и код заказа), а также хотя бы один товар (два поля: код и кол-во).')
-  else if (count mod 2 = 1) then append_err(err_string, 'ФОРМАТ ДАННЫХ: Неверное количество данных. Товары имеют по два поля: код и кол-во. У последнего товара указан только код.');
+  
   Result := err_string;
 end;
 
